55b3ec08d3fe4275f5093043cb4cd49b9f37e9e1
Mathéo Martin <matheo.martin@epitech.eu>
2023-11-21 10:03:03 +0100

    Refactor job_dsl.groovy

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -1,14 +1,17 @@
+// Define folders
 folder("Whanos base images") {
     description("The base images of whanos.")
 }
 
 folder("Projects") {
-    description("The available projets in whanos.")
+    description("The available projects in whanos.")
 }
 
-languages = ["c", "java", "javascript", "python", "befunge", "cpp", "brainfuck"]
+// Language array
+def languages = ["c", "java", "javascript", "python", "befunge", "cpp", "brainfuck"] as java.lang.Object
 
-languages.each { language ->
+// Function to create freeStyleJob for a language
+def createLanguageJob(language) {
     freeStyleJob("Whanos base images/whanos-$language") {
         steps {
             shell("docker build /images/$language -f /images/$language/Dockerfile.base -t whanos-$language")
@@ -16,14 +19,19 @@ languages.each { language ->
     }
 }
 
+// Create jobs for each language
+languages.each { language ->
+    createLanguageJob(language)
+}
+
+// Create a job to build all base images
 freeStyleJob("Whanos base images/Build all base images") {
     publishers {
-        downstream(
-                languages.collect { language -> "Whanos base images/whanos-$language" }
-        )
+        downstream(languages.collect { "Whanos base images/whanos-$it" })
     }
 }
 
+// Create a job to link a project
 freeStyleJob("link-project") {
     parameters {
         stringParam("GIT_URL", null, 'Git repository url')
@@ -31,27 +39,26 @@ freeStyleJob("link-project") {
     }
     steps {
         dsl {
-            text('''
-				freeStyleJob("Projects/$DISPLAY_NAME") {
-					scm {
-						git {
-							remote {
-								name("origin")
-								url("$GIT_URL")
-							}
-						}
-					}
-					triggers {
-						scm("* * * * *")
-					}
-					wrappers {
-						preBuildCleanup()
-					}
-					steps {
-						shell("/jenkins/deploy.sh \\"$DISPLAY_NAME\\"")
-					}
-				}
-			''')
+            text('''freeStyleJob("Projects/$DISPLAY_NAME") {
+                    scm {
+                        git {
+                            remote {
+                                name("origin")
+                                url("$GIT_URL")
+                            }
+                        }
+                    }
+                    triggers {
+                        scm("* * * * *")
+                    }
+                    wrappers {
+                        preBuildCleanup()
+                    }
+                    steps {
+                        shell("/jenkins/deploy.sh \\"$DISPLAY_NAME\\"")
+                    }
+                }
+            ''')
         }
     }
 }
